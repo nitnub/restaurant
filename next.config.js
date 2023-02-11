1aa8b71a0c0cbf869c9c7aac4e225bd0db0f77e6
nitnub <45341924+nitnub@users.noreply.github.com>
2023-02-11 09:54:32 -0500

    update env link

--- next.config.js
+++ next.config.js
@@ -17,8 +17,8 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
+    // Testing: allows production builds to successfully complete even if
+    // project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   output: 'standalone',
@@ -26,7 +26,7 @@ const nextConfig = {
     return [
       {
         source: '/api/graphql',
-        destination: process.env.AWS_URL,
+        destination: process.env.NEXT_PUBLIC_RESOURCE_PATH,
       },
     ];
   },
